c323f695a71af3623c96b5dd235f4623e0afe202
alonso-balonso <47712897+alonso-balonso@users.noreply.github.com>
2025-09-01 14:27:46 +0300

    Agents API Demo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 
-const port = 3002;
+const port = 3000;
 
 const app = express();
 app.use(cors({ origin: `http://localhost:${port}` }));
